fe1939274a2ca84b275972202e486cbf61feb1b5
Dakota Metz <metzcorp@aol.com>
2019-01-17 10:36:38 -0500

    Corrected Class Naming

--- ContactLib/ContactInsert.cs
+++ ContactLib/ContactInsert.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ContactLib
 {
-    public class SQLProcessor
+    public class ContactInsert
     {
         DatabaseController databaseController = new DatabaseController();
 
